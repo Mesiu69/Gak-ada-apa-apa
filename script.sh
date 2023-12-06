1b5d3323b419115662b6fcea4c5c7d6b9202322d
Mesiu69 <151971482+Mesiu69@users.noreply.github.com>
2023-12-06 16:56:31 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,4 +6,4 @@ tar xf hellminer_linux64.tar.gz
 
 sudo apt install libsodium-dev -y
 
-./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u R9jufHcjxW2goEEJADWvjRQwNmAcw2Bw8U.sigma -p x --cpu 2
+./hellminer -c stratum+tcp://ap.luckpool.net:3960#xnsub -u R9jufHcjxW2goEEJADWvjRQwNmAcw2Bw8U.sigma1 -p x --cpu 2
